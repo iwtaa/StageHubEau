ecac51171d4e4bd6071f7aa44c5585abc422aa03
iwtaa <71445121+iwtaa@users.noreply.github.com>
2025-05-19 11:11:40 +0200

    Exemple better colors + colorbar

--- src/PH_charente_maritime.py
+++ src/PH_charente_maritime.py
@@ -1,9 +1,18 @@
+import colorsys
 from APIcalls import *
-import json
 import numpy as np
-from collections import Counter
 
-data = getMeasureDepartment('16', parameter='1302', date_max_prelevement='2025-01-01 00:00:00', date_min_prelevement='2021-08-01 00:00:00')
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from APIcalls import *
+import pprint
+
+departement = '17'
+parameter = '1340'
+date_max_prelevement = '2025-01-01 00:00:00'
+date_min_prelevement = '2024-08-01 00:00:00'
+
+data = getMeasureDepartment(departement, parameter=parameter, date_max_prelevement=date_max_prelevement, date_min_prelevement=date_min_prelevement)
 
 average_communes = {}
 communes = {}
@@ -15,18 +24,8 @@ for measure in data['data']:
 
 average_communes = {key: np.mean(value) for key, value in communes.items()}
 
-
-
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import random
-from APIcalls import *
-import pprint
-
-
-
 try:
-    data = getAPIdata('https://geo.api.gouv.fr/departements/16/communes?format=geojson&geometry=contour')
+    data = getAPIdata(f'https://geo.api.gouv.fr/departements/{departement}/communes?format=geojson&geometry=contour')
     gdf = gpd.GeoDataFrame.from_features(data['features'])
     # Define a function to determine the color of each geometry
     def get_color(code):
@@ -34,18 +33,22 @@ try:
             number = 0.0
         else:
             number = average_communes[code]
-        min_val = 7.0
-        max_val = 9.0
-        if min_val <= number <= max_val:
+        min_val = 0.1
+        max_val = 50.0
+        if number == 0.0:
+            return 'black'
+        elif number < min_val:
+            return 'blue'
+        elif number > max_val:
+            return 'red'
+        else:
             # Normalize the value to a range between 0 and 1
             normalized_value = (number - min_val) / (max_val - min_val)
-            # Interpolate between green and red based on the normalized value
-            red = normalized_value
-            green = 1 - normalized_value
-            blue = 0  # No blue component
-            return (red, green, blue)
-        else:
-            return 'black'  # Default color
+            # Convert the normalized value to a hue value (0=red, 1/6=yellow, 1/3=green)
+            hue = (0.33) - (normalized_value * (0.33))
+            # Convert the HSL color to RGB
+            r, g, b = colorsys.hls_to_rgb(hue, 0.5, 1)  # Use HSL: Hue, Saturation, Lightness
+            return (r, g, b)
 
     # Apply the function to each geometry to get its color
     gdf['color'] = gdf.apply(lambda row: get_color(row['code']), axis=1)
@@ -53,7 +56,23 @@ try:
     pprint.pprint(gdf.head())  # Print the first few rows of the GeoDataFrame
     # Generate random colors for each area
     num_geometries = len(gdf)
-    gdf.plot(color=gdf['color'], legend=True)
+    
+    # Create a figure and an axes object
+    fig, ax = plt.subplots(1, 1)
+
+    # Plot the GeoDataFrame
+    gdf.plot(color=gdf['color'], ax=ax)
+
+    # Create a colorbar
+    import matplotlib.colors as mcolors
+    cmap = mcolors.LinearSegmentedColormap.from_list("mycmap", ["green", "yellow", "red"])
+
+    # Add a colorbar to the plot
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0.1, vmax=50))
+    sm._A = []  # Fix for the ScalarMappable error
+    cbar = fig.colorbar(sm, ax=ax)
+    cbar.set_label('Value')
+    
     plt.show()
 
 except FileNotFoundError:
