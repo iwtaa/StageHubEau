4bb120919c70f225249df97c5f0ac5af8642c6b3
iwtaa <71445121+iwtaa@users.noreply.github.com>
2025-05-28 16:13:26 +0200

    added more processed values

--- src/process.py
+++ src/process.py
@@ -1,18 +1,24 @@
 from parameter_plot import *
 import pandas as pd
 from tqdm import tqdm
-
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 def main():
     files = load_data_list()
-    print(f"Found {len(files)} files to process.")
+    files = files[::-1]
     for file in tqdm(files):
         pdf = load_and_prepare_data(file)
         pdf = center_reduce(pdf)
         std_devs = pdf.groupby("inseecommuneprinc")["valtraduite"].std()
-        pdf["stdcommune"] = pdf.apply(lambda row: row["valtraduite"] + std_devs[row["inseecommuneprinc"]], axis=1)
+        pdf["stdcommune"] = pdf["inseecommuneprinc"].map(std_devs)
         mean_vals = pdf.groupby("inseecommuneprinc")["valtraduite"].mean()
-        pdf["meancommune"] = pdf.apply(lambda row: row["valtraduite"] + mean_vals[row["inseecommuneprinc"]], axis=1)
+        pdf["meancommune"] = pdf["inseecommuneprinc"].map(mean_vals)
+        global_mean = pdf["valtraduite"].mean()
+        global_std = pdf["valtraduite"].std()
+        pdf["deregionalized_valtraduite"] = (pdf["centered_reduced_val"] * global_std) + global_mean
+        pdf = smooth(pdf, col='deregionalized_valtraduite', window_size=60)
+        pdf = smooth(pdf, col='centered_reduced_val', window_size=60)
         file_name = file.split(".")[0]
         pdf.to_csv(f"{file_name}_processed.csv")
 
